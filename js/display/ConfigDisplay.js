8212f3f9c3e5e7c2d824fbcd6dc92646cbabba71
jftoh <tohjianfeng@gmail.com>
2017-04-15 20:21:52 +0800

    Added function to save config to local file

--- js/display/ConfigDisplay.js
+++ js/display/ConfigDisplay.js
@@ -3,8 +3,12 @@ function ConfigDisplay () {
 	this.displayContent = document.getElementById( 'config-content' );
 	this.displayOverlay = document.getElementById( 'config-overlay' );
 	this.serverButton = document.getElementById( 'save-server' );
+	this.localButton = document.getElementById( 'save-local' );
+
+	this.savelink = document.createElement( 'a' );
 
 	this.serverButton.onclick = () => this.sendFileToServer( this.displayContent.innerHTML );
+	this.localButton.onclick = () => this.saveToLocalFile( this.displayContent.innerHTML  );
 }
 
 ConfigDisplay.prototype = ( function () {
@@ -26,17 +30,23 @@ ConfigDisplay.prototype = ( function () {
 		sendFileToServer: function ( text ) {
 			var xmlreq = new XMLHttpRequest();
 
-			
 			xmlreq.onreadystatechange = function () {
 				if ( xmlreq.readyState === 4 && xmlreq.status === 200 ) {
 					console.log( xmlreq.responseText );
 				}
 			};
-			
 
 			xmlreq.open( "POST", "filehandler.php", true );
 			xmlreq.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" );
 			xmlreq.send( "config=" + JSON.stringify(text) );
+		},
+
+		saveToLocalFile: function ( json ) {
+			let timestamp = Math.floor( Date.now() );
+			let uriContent = 'data:text/plain;charset=utf-8,' + encodeURIComponent( json );
+			this.savelink.setAttribute( 'href', uriContent );
+			this.savelink.setAttribute( 'download', 'config_' + timestamp + '.json');
+			this.savelink.click();
 		}
 	};
 } )();
